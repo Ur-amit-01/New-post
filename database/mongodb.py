bce900944020473b84a617ef4927b38fba07d3e1
Dranuragmaurya <162044160+Dranuragmaurya@users.noreply.github.com>
2025-05-18 22:11:57 +0530

    Add files via upload

---
+++ database/mongodb.py
@@ -0,0 +1,17 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import Config
+import logging
+
+logger = logging.getLogger(__name__)
+
+try:
+    client = AsyncIOMotorClient(Config.MONGO_URI)
+    db = client['channel_bot']
+    channels = db['channels']
+    
+    # Test the connection
+    client.admin.command('ping')
+    logger.info("MongoDB Connected Successfully!")
+except Exception as e:
+    logger.error(f"Error connecting to MongoDB: {e}")
+    raise Exception("MongoDB Connection Failed!")
