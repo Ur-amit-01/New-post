7d2b1bf041342ea7d38014ef78574b19fecc1a32
Amit <heroku467@gmail.com>
2025-05-20 17:26:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-from os import getenv
-from dotenv import load_dotenv
+import re, os
+from os import environ
 
-load_dotenv()
+id_pattern = re.compile(r'^.\d+$') 
 
-class Config:
-    API_ID = int(getenv("API_ID"))
-    API_HASH = getenv("API_HASH")
-    BOT_TOKEN = getenv("BOT_TOKEN")
-    MONGO_URI = getenv("MONGO_URI")
+API_ID = os.environ.get("API_ID", "22012880")
+API_HASH = os.environ.get("API_HASH", "5b0e07f5a96d48b704eb9850d274fe1d")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8113642693:AAG9yJpZjyhKIP_nhsIIoc8ZsiTJ-gsudLU")
+MONGO_URI = os.environ.get("DB_URL","mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
